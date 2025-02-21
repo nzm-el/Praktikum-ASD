77294ed740e68dfe31382109a525c6a5163f4954
Nizam El <nizamel1402@gmail.com>
2025-02-21 14:26:11 +0700

    Add files via upload

--- Jobsheet2/MahasiswaMain14.java
+++ Jobsheet2/MahasiswaMain14.java
@@ -1,7 +1,7 @@
 public class MahasiswaMain14 {
     public static void main(String[] args) {
         Mahasiswa14 mhs1 = new Mahasiswa14();
-        mhs1. nama = "Muhammad Ali Farhan";
+        mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
@@ -10,5 +10,6 @@ public class MahasiswaMain14 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
     }
 }
