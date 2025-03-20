bca1990c537c68e7faa39e8dffa98c40a7300496
Nizam El <nizamel1402@gmail.com>
2025-03-20 20:03:19 +0700

    modif pert 2

--- Jobsheet5/Faktorial16.java
+++ Jobsheet5/Faktorial16.java
@@ -1,9 +1,17 @@
 public class Faktorial16 {
+
+    int faktorialBF, factorialDC;
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while(i < n) {
+            fakto *= i;
+            i++;
         }
+        
+        // for (int i = 1; i <= n; i++) {
+        //     fakto = fakto * i;
+        // }
         return fakto;
     }
 
