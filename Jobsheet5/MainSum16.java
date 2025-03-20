7dd48f0c1567eb199c6ea3deac519f6431d61e41
Nizam El <nizamel1402@gmail.com>
2025-03-20 20:44:41 +0700

    ..

---
+++ Jobsheet5/MainSum16.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MainSum16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah element : ");
+        int elemen = input.nextInt();
+
+        Sum16 sm = new Sum16(elemen);
+        for(int i = 0; i < elemen; i++ ) {
+            System.out.print("Masukkan keuntungan ke-" + (i + 1) + ": ");
+            sm.keuntungan[i] = input.nextDouble();
+        }
+
+        System.out.println("Total keuntungan menggunakan Bruteforce : " + sm.totalBF());
+        System.out.println("Total keuntungan menggunakan Devide and Conquer : " + sm.totalDC(sm.keuntungan, 0, elemen-1));
+
+    }
+}
